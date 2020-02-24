dfb41df5f63ee9ae52848bf4d23d60ec60d0a230
Josealfaro4 <joalfaro@csumb.edu>
2020-02-24 07:16:09 +0000

    updated the files

--- Assignment2/main.js
+++ Assignment2/main.js
@@ -1,20 +1,3 @@
-//Set of questions and answers -DONE
-//Give each answer an identifier -DONE
-//Each identifier will increment through each question
-//St the end the identifier with the highest number is the winner 
-//Display the answer and rest the quiz
-
-//pass results frm previous question to the next page usig localcache
-
-//Randomise the background of the quiz for each questiion
-
-//Possible - Personality Traits
-// 15 -21- You Need Help
-// 10 - 15 - Good Soul
-// 5- 10 - Meh 
-// 5 - Are You Even Real
-
-
 let currentQuestion = 0;
 let score = [];
 let selectedAnswersData = [];
@@ -30,14 +13,12 @@ const previousButton = document.querySelector('.previous');
 const restartButton = document.querySelector('.restart');
 const result = document.querySelector('.result');
 
-//Function to generate question 
 function generateQuestions (index) {
-    //Select each question by passing it a particular index
     const question = questions[index];
     const option1Total = questions[index].answer1Total;
     const option2Total = questions[index].answer2Total;
     const option3Total = questions[index].answer3Total;
-    //Populate html elements 
+
     questionEl.innerHTML = `${index + 1}. ${question.question}`
     option1.setAttribute('data-total', `${option1Total}`);
     option2.setAttribute('data-total', `${option2Total}`);
@@ -55,10 +36,8 @@ function loadNextQuestion () {
         alert('Please select your answer!');
         return;
     }
-    //Get value of selected radio
     const answerScore = Number(selectedOption.nextElementSibling.getAttribute('data-total'));
 
-    ////Add the answer score to the score array
     score.push(answerScore);
 
     selectedAnswersData.push()
@@ -66,12 +45,9 @@ function loadNextQuestion () {
 
     const totalScore = score.reduce((total, currentNum) => total + currentNum);
 
-    //Finally we incement the current question number ( to be used as the index for each array)
     currentQuestion++;
 
-        //once finished clear checked
         selectedOption.checked = false;
-    //If quiz is on the final question
     if(currentQuestion == totalQuestions - 1) {
         nextButton.textContent = 'Finish';
     }
@@ -83,10 +59,9 @@ function loadNextQuestion () {
          <div class="summary">
             <h1>Summary</h1>
             <p>Possible - Personality Traits, see below for a summary based on your results:</p>
-            <p>15 - 21- You Need Help</p>
-            <p>10 - 15 - Good Soul</p>
-            <p>5 - 10 - Meh </p>
-            <p>5 - Are You Even Real</p>
+            <p> 16 - 21 - You're an extrovert!</p>
+            <p> 12 - 15 - You're a bit of both!</p>
+            <p> 7 - 11 - You're an introvert! </p>
         </div>
         <button class="restart">Restart Quiz</button>
          `;
@@ -97,26 +72,20 @@ function loadNextQuestion () {
 
 //Function to load previous question
 function loadPreviousQuestion() {
-    //Decrement quentions index
     currentQuestion--;
-    //remove last array value;
     score.pop();
-    //Generate the question
     generateQuestions(currentQuestion);
 }
 
 //Fuction to reset and restart the quiz;
 function restartQuiz(e) {
     if(e.target.matches('button')) {
-    //reset array index and score
     currentQuestion = 0;
     score = [];
-    //Reload quiz to the start
     location.reload();
     }
 
 }
-
 
 generateQuestions(currentQuestion);
 nextButton.addEventListener('click', loadNextQuestion);
--- Assignment2/questions.js
+++ Assignment2/questions.js
@@ -1,15 +1,33 @@
 const questions = [
   {
-    "question": "Age range?",
-    "answer1": "under 18",
-    "answer1Total": "1",
-    "answer2": "18 - 30",
+    "question": "You enjoy going out with friends.",
+    "answer1": "Agree",
+    "answer1Total": "3",
+    "answer2": "Neutral",
     "answer2Total": "2",
-    "answer3": "over 30",
-    "answer3Total": "3"
+    "answer3": "Disagree",
+    "answer3Total": "1"
   },
   {
-    "question": "I am very imaginative.",
+    "question": "You like speaking in public.",
+    "answer1": "Agree",
+    "answer1Total": "3",
+    "answer2": "Neutral",
+    "answer2Total": "2",
+    "answer3": "Disagree",
+    "answer3Total": "1"
+  },
+  {
+    "question": "You like being a leader.",
+    "answer1": "Agree",
+    "answer1Total": "3",
+    "answer2": "Neutral",
+    "answer2Total": "2",
+    "answer3": "Disagree",
+    "answer3Total": "1"
+  },
+  {
+    "question": "You prefer staying inside.",
     "answer1": "Agree",
     "answer1Total": "1",
     "answer2": "Neutral",
@@ -18,52 +36,31 @@ const questions = [
     "answer3Total": "3"
   },
   {
-    "question":
-      "Select in which order you would value these \"Money, Love & Career",
-    "answer1": "Love, Career, Money",
+    "question": "You hate when teachers call on you.",
+    "answer1": "Agree",
     "answer1Total": "1",
-    "answer2": "Money, Career, Love",
-    "answer2Total": "3",
-    "answer3": "Career, Love, Money",
-    "answer3Total": "2"
-  },
-  {
-    "question": "Best Sentence to describe you?",
-    "answer1": "You feel superior to other people.",
-    "answer1Total": "3",
-    "answer2": "You consider yourself more practical than creative.",
+    "answer2": "Neutral",
     "answer2Total": "2",
-    "answer3":
-      "Winning a debate matters less to you than making sure no one gets upset.",
-    "answer3Total": "1"
-  },
-  {
-    "question": "Which best describes your relationship with food",
-    "answer1": "You tend to over-eat when you have company.",
-    "answer1Total": "1",
-    "answer2": "You tend to eat snacks secretly.",
-    "answer2Total": "2",
-    "answer3": "You prepare food and don\’t even look at the recipe.",
+    "answer3": "Disagree",
     "answer3Total": "3"
   },
   {
-    "question":
-      "You make plans with a friend and they cancel on you, what do you do?",
-    "answer1":
-      "Say \"whatever\" and plan a night that'll be GREAT so they don't cancel again.",
+    "question": "You enjoy being around people.",
+    "answer1": "Agree",
     "answer1Total": "3",
-    "answer2": "Feel hurt because you were looking forward to tonight.",
+    "answer2": "Neutral",
     "answer2Total": "2",
-    "answer3": "No problem, you kinda wanted to stay home anyway.",
+    "answer3": "Disagree",
     "answer3Total": "1"
   },
   {
-    "question": "Which of the following colours do you like most?",
-    "answer1": "Black",
+    "question": "You are independent.",
+    "answer1": "Agree",
     "answer1Total": "1",
-    "answer2": "Yellow or light blue",
+    "answer2": "Neutral",
     "answer2Total": "2",
-    "answer3": "Red or orange",
+    "answer3": "Disagree",
     "answer3Total": "3"
   }
+  
 ]
